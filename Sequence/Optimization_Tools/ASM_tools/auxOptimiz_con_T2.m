1602d7d50366461cc5bb0e7dfb045c245b95112c
ZemaTimoteo <37109227+ZemaTimoteo@users.noreply.github.com>
2024-09-02 12:32:16 +0100

    Delete Sequence/Optimization_Tools directory

--- Sequence/Optimization_Tools/ASM_tools/auxOptimiz_con_T2.m
+++
@@ -1,6 +0,0 @@
-function [c1,c1eq,gradc1,gradc1eq] = auxOptimiz_con_T2(x,constVariables)
-
-    % parameters
-
-
-end
--- Sequence/Optimization_Tools/ASM_tools/blockdiag_mult_ASM.m
+++
@@ -1,8 +0,0 @@
-
-function w = blockdiag_mult_ASM(A,v)
-    %given A square matrix, it returns   ' w =  kron(A,eye(nA))*v  '   efficiently
-    nA = size(A,1); %square
-    V = reshape(v,nA,length(v)/nA);
-    W = A*V;
-    w = W(:);
-end
